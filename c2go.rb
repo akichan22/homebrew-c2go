9d5a11bc96ba4991206d4b651a457f14ca6c7142
ch1aki <sugawara256@gmail.com>
2019-08-12 18:05:12 +0900

    v0.2.0

--- c2go.rb
+++ c2go.rb
@@ -1,13 +1,13 @@
 require 'formula'
 
 REPOSITORY_URL='https://github.com/ch1aki/c2go'
-HOMEBREW_C2GO_VERSION='0.1.0'
+HOMEBREW_C2GO_VERSION='0.2.0'
 
 class C2go < Formula
   desc 'Visualizing `uniq -c` result'
   homepage REPOSITORY_URL
   url "#{REPOSITORY_URL}/releases/download/v#{HOMEBREW_C2GO_VERSION}/c2go_#{HOMEBREW_C2GO_VERSION}_darwin_amd64.zip", :tag => "v#{HOMEBREW_C2GO_VERSION}"
-  sha256 '16764cc809560f4defc23db1b78bcfe3ca5ef4f374226db660205eb4f2ab35b2'
+  sha256 'd0197183e42d6e00c12fa1d55507e1d62985c57dbefa8ccbdd0ecd71c784bea9'
   head REPOSITORY_URL, :branch => 'master'
   version HOMEBREW_C2GO_VERSION
 
